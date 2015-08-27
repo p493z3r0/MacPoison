d878ec7776e3bcba389936cfa3c4e5fd8f7c6bd3
Dominic12 <Dominic.jaermann@icloud.com>
2015-08-27 12:45:54 +0200

    Create RemoteShell.sh

---
+++ RemoteShell.sh
@@ -0,0 +1,6 @@
+echo this is an example, how to wait for an remote shell
+echo NetCat must be installed
+echo chance the port to the same as in the exploit
+echo nc -l -p <port> 
+echo starting listening..
+nc -l -p 1337
